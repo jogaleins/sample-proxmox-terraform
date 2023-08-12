a5c8b59a07c12cc215d7298572f9d112016b1783
ap <adpazon@gmail.com>
2023-08-12 23:58:34 +0800

    added ansible

---
+++ .terraform.lock.hcl
@@ -0,0 +1,24 @@
+# This file is maintained automatically by "terraform init".
+# Manual edits may be lost in future updates.
+
+provider "registry.terraform.io/bpg/proxmox" {
+  version     = "0.29.0"
+  constraints = "0.29.0"
+  hashes = [
+    "h1:k19Jlz3l19b9G0jO5dPJd7LMRCNRLBtfgept8r8+Rhs=",
+    "zh:17c4de58917c52c4a73c698dae743719173c032ac3980d9f0271da45034952cb",
+    "zh:1a8788fd2fde195a3c88da9e94620e6a058b9c6b402a68c73ec4fafbb1b911f0",
+    "zh:1ce8e787ce3e4ff740af0bdafa3297302e30c0d51616560a7dabc86ecb7de361",
+    "zh:285c9cb8c14fe7ae0fecb65c854f1d29b72160daa1175376afe33e33aaaf94f0",
+    "zh:3e016b592bd4a88c7a477528008cdf27c87e183f91d052b94d8bc18e16e29d9d",
+    "zh:428bb1593de113088a05b33d326a68f292bcd811e955316768278a9e961cb3a9",
+    "zh:48f1522843db8f162697513c9d86812e4da4313f7d0f9103306d282a428e9df2",
+    "zh:76250988b17ed0ed3aae06d6d80e00b56824c2dd3efddbfc7e1fa0903ecd23c3",
+    "zh:76bb6f611e6f3ce2bd772e70984d0455f5d6dd70ab2aea5f78116627ae153e8a",
+    "zh:88b9b690055023a4b0c5003f92b7062c9c65152126ad23a580e8af8807dac449",
+    "zh:acdc04b0c8db1768d211646bdeda67b76c8e3d7b16d8c684617313263ca57f47",
+    "zh:d2bfe958ae58a47757b9358fa0a1a892003850ceb1a4a40c7b81fa63ee027856",
+    "zh:d74779b84956c1f5b353d3fe3a4ff967c2fe5d2b45f20a4021fb0ffcee017837",
+    "zh:df25f468505d2d571648deb4d76f04b5edeeb693efe1e92c6fdef8a8b83ee031",
+  ]
+}
--- alpine.tf
+++ alpine.tf
@@ -15,9 +15,14 @@ provider "proxmox" {
   insecure = true
 }
 
+variable "ip_list" {
+  type = list(string)
+  description = "Proxmox VM IP"
+  default     = ["192.168.2.211/24", "192.168.2.212/24", "192.168.2.213/24"]
+}
 resource "proxmox_virtual_environment_vm" "alpine_vm" {
-  count = 2
-  name        = "test-alpine-vm-${count.index + 1}"
+  count = 3
+  name        = "k3s-agent-${count.index + 1}"
   description = "Managed by Terraform"
   tags        = ["terraform", "alpine"]
   
@@ -41,7 +46,7 @@ resource "proxmox_virtual_environment_vm" "alpine_vm" {
 
   clone {
     datastore_id = "pve-ssd"
-    vm_id = 9988
+    vm_id = 8888
   }
   
   operating_system {
@@ -54,10 +59,15 @@ resource "proxmox_virtual_environment_vm" "alpine_vm" {
     datastore_id = "pve-ssd"
     ip_config {
       ipv4 {
-        address = "192.168.2.214+${count.index}/24"
+        address = element(var.ip_list, count.index)
         gateway = "192.168.2.1"
       }
     }
 
+    user_account {
+      keys     = ["ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABgQC6jQt2h2sOr5Q2v+2P4+4Fmf3IeILavNT3ihltZWW+XD+IeIpX6MaUe7EzzqpvAaLhVU+z+rRZO+EteoOGWTrAmcF+RKnkJAWeNnEeDitrshepffkB7Bw20XKiQi7B7UeXJipwhsJnQAZjoITx7VtylS+j/ruYJSRaq7gOxUPMlvJRc1W4kPmQ3D/EyvSrlkglngs2aP0D/Wl+sQW2S9XxucjQPloJugBQkjdQrDW67SukpOjWN+04iXlYsDvN/XFwOabqc7ZeuaD//TUkd/G6l9n/9yjtnLO8Z883BT6NNXc35r4fybzNsT1rQ4q9Xg+wahtHzH3PtZUGBzLEadN/hwNKkblWdaCdhx4fxqD8HAy24xgUuCFpvz2QBkHqFBTB/NU3sEsnrUED2EE3QrVXXxb6MUXzp2/+M6JhgkWfni6Ua+o7L2EMQ4MSGcSWbTnijiEtjRPbQrgRnpjq/rny8rzhEpt9OMLRnI+H9E00S+NO18vcP2fcj7QF+RKYdnE= ap@DESKTOP-NIL7Q34","ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQDkRIQkJQz86g4y3K8VpvwwJgPoq39SvPraXoVclfWHfw52SeMaSamaryTALzrQ5szLl/Q/jRbVTjj2pGH3zBn2T1KVZyVVl3+hI6DytP6i0fv51/R8IOQ0bl3yvlBkG/FZPPkE0xtAR9ymuzbqzv2yICnl6PVA6JnGV80+Zde+b15nPUh1sGW5Dq/lsjxXBNaheGF40Ba/AF1UGDOAt4i99+riEkNPt/SMCSv9o4MIjpjip2Y2/JMhOw02bSzIg8ddXZZ37VAaE0oWSn/c2jxUW9VZddFlIn8PagKXafmfNCb4adBUeRk2rHI+KcE9Vnmxu9YyB11kShA+ChCzVS99 ap@docker"]
+      password = "soeid1"
+      username = "alpine"
+    }
 }
 }
