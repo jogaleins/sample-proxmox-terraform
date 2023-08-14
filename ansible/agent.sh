43ce28d01ec5c8501acbf80c1f2e314a9f38b1da
ap <adpazon@gmail.com>
2023-08-14 22:15:04 +0800

    err

--- ansible/agent.sh
+++ ansible/agent.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 
-curl -sfL https://get.k3s.io | K3S_TOKEN=SECRET sh -s - server --server https://192.168.2.211:6443 --flannel-iface=eth0 --disable servicelb
+export INSTALL_K3S_VERSION="v1.26.7+k3s1"
+curl -sfL https://get.k3s.io | INSTALL_K3S_VERSION="v1.26.7+k3s1" K3S_TOKEN=SECRET sh -s - server --server https://192.168.2.211:6443 --flannel-iface=eth0 --disable servicelb
--- ansible/master.sh
+++ ansible/master.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-
-curl -sfL https://get.k3s.io | K3S_TOKEN=SECRET sh -s - server --cluster-init --flannel-iface=eth0 --disable servicelb
+export INSTALL_K3S_VERSION="v1.26.7+k3s1"
+curl -sfL https://get.k3s.io | INSTALL_K3S_VERSION="v1.26.7+k3s1" K3S_TOKEN=SECRET sh -s - server --cluster-init --flannel-iface=eth0 --disable servicelb
 sleep 60
